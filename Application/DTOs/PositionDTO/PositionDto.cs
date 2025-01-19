bdf2c2df614ceead8d5ad13c96ec9f83d19ec16a
Albert <albertjohan2707@gmail.com>
2025-01-19 16:41:38 -0400

    feat: adding entities DTOs

---
+++ Application/DTOs/PositionDTO/PositionDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Position
+{
+    public class PositionDto
+    {
+        public int PositionId { get; set; }
+        public string Name { get; set; }
+    }
+}
--- Application/DTOs/UpdateProductDto.cs
+++ Application/DTOs/UpdateProductDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,13 +9,13 @@ namespace Application.DTOs
 {
     public class UpdateProductDto
     {
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
         public decimal Price { get; set; }
-        public string ImgUrl { get; set; } = string.Empty;
-
         public int Stock { get; set; }
         public int CategoryId { get; set; } // ID de la categoría
         public int ProductTypeId { get; set; }
+        public IFormFile file { get; set; }
+
     }
 }
