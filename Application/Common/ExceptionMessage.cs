91e0ed1d02912e1271bb6c19b28e43e7a6621710
Albert <albertjohan2707@gmail.com>
2025-01-19 16:41:03 -0400

    feat: class to handle exceptions

---
+++ Application/Common/ExceptionMessage.cs
@@ -0,0 +1,18 @@
+﻿using Infrastructure.Data.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Common
+{
+    public static class ExceptionMessage
+    {
+        public static void CatchException(Exception ex)
+        {
+            Logs.Error(ex.ToString());
+            throw ex;
+        }
+    }
+}
